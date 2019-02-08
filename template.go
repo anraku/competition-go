c863a4c8bc7b878cf20178a236d7bb3ab1bc6bf7
anraku <anraku90@gmail.com>
2019-02-08 16:26:09 +0900

    modify template

--- template.go
+++ template.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -12,13 +13,17 @@ import (
 func main() {
 	log.SetFlags(log.Lshortfile)
 	nextReader = NewScanner()
-	a := nextInt()
-	b := nextInt()
-	c := nextInt()
+	line1 := nextInt()
+	line2 := nextInt()
+	sort.Sort(IntSorter(line2))
+	line3 := nextFloat64()
+	sort.Sort(Float64Sorter(line3))
+	x := line2
 
-	fmt.Printf("a = %d\nb = %d\nc = %d\n", a, b, c)
+	fmt.Printf("line1= %+v\nx = %+v\nline3=%+v\n", line1, x, line3)
 }
 
+// ------ Mathライブラリ ---------------------------------//
 func max(a ...int) int {
 	r := a[0]
 	for i := 0; i < len(a); i++ {
@@ -77,36 +82,70 @@ func abs(a int) int {
 	return a
 }
 
-var nextReader func() string
+// ------ Mathライブラリ ---------------------------------//
 
-func NewScanner() func() string {
+// ----- 標準入力用の関数 ----------------------------------//
+var nextReader func() []string
+
+func NewScanner() func() []string {
 	r := bufio.NewScanner(os.Stdin)
 	r.Buffer(make([]byte, 1024), int(1e+11))
-	r.Split(bufio.ScanWords)
-	return func() string {
+	return func() []string {
 		r.Scan()
 		return strings.Fields(r.Text())
 	}
 }
-func nextString() string {
+
+func nextString() []string {
 	return nextReader()
 }
-func nextInt64() int64 {
-	v, _ := strconv.ParseInt(nextReader(), 10, 64)
-	return v
-}
-func nextInt() int {
-	v, _ := strconv.Atoi(nextReader())
-	return v
-}
-func nextInts(n int) []int {
-	r := make([]int, n)
-	for i := 0; i < n; i++ {
-		r[i] = nextInt()
+
+func nextInt() []int {
+	var intArray []int
+	strArray := nextReader()
+	for _, v := range strArray {
+		i, _ := strconv.Atoi(v)
+		intArray = append(intArray, i)
 	}
-	return r
+	return intArray
 }
-func nextFloat64() float64 {
-	f, _ := strconv.ParseFloat(nextReader(), 64)
-	return f
+
+func nextFloat64() []float64 {
+	var floatArray []float64
+	strArray := nextReader()
+	for _, v := range strArray {
+		f, _ := strconv.ParseFloat(v, 64)
+		floatArray = append(floatArray, f)
+	}
+	return floatArray
 }
+
+// ------ 標準入力用の関数 ---------------------------------//
+
+// ------ ソートライブラリ ---------------------------------//
+type IntSorter []int
+
+func (s IntSorter) Len() int           { return len(s) }
+func (s IntSorter) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s IntSorter) Less(i, j int) bool { return s[i] < s[j] }
+
+type Float64Sorter []float64
+
+func (s Float64Sorter) Len() int           { return len(s) }
+func (s Float64Sorter) Swap(i, j int)      { s[i], s[j] = s[j], s[i] }
+func (s Float64Sorter) Less(i, j int) bool { return s[i] < s[j] }
+
+// ------ ソートライブラリ ---------------------------------//
+
+// ------ あまり使わない -----------------------------------//
+func nextInt64() []int64 {
+	var int64Array []int64
+	strArray := nextReader()
+	for _, v := range strArray {
+		i, _ := strconv.ParseInt(v, 10, 64)
+		intArray = append(intArray, i)
+	}
+	return intArray
+}
+
+// ------ あまり使わない -----------------------------------//
